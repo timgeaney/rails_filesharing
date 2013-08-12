77783faed4596b7b005a04deed72ebe184663eff
tim geaney <timgeaney24@gmail.com>
2013-08-12 11:57:24 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsFileshare::Application.config.secret_token = '8316f22909831ecc7eca98d7e4643942d7ef84e311ed245ad881ec465ebaccff26eb3b9ea2bfd81960241b1e4c1dccaa8e0b314eec8fecd469e83e8c85a9f0a2'
