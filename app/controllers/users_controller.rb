3e967e4760ad2a93aa0d91701d4e3e86f05d5539
tim geaney <timgeaney24@gmail.com>
2013-08-12 13:05:23 +0100

    restrict access to admins in user controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
+  before_filter :authenticate_user!
+
   def index
+    authorize! :index, @user, :message => 'Not authorized as an Administrator.'
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
   end
 end
