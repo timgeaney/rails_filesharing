33393d444cf93b6ecbcf9a2e754fa86e4f3d011a
tim geaney <timgeaney24@gmail.com>
2013-08-12 12:40:37 +0100

    added user management

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,32 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise  :database_authenticatable, 
+          :registerable,
+          :recoverable, 
+          :rememberable, 
+          :trackable, 
+          :validatable,
+          :token_authenticatable,
+          :omniauthable
 
-  # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  attr_accessible :role_ids, :as => :admin       
+  
+  attr_accessible :avatar, 
+                  :firstname,
+                  :lastname, 
+                  :username,
+                  :email, 
+                  :password, 
+                  :password_confirmation, 
+                  :remember_me
+  
+  has_attached_file :avatar, { :styles => {:medium => "300 x 240", :thumb => "112x112#", :mini => '44x44#'}, :default_url => "/images/:style/missing.png" }
+
+  #validates_presence_of :firstname, :lastname
+  validates_uniqueness_of :email, :case_sensitive => false
+
+  has_many :groups
+  has_many :trips, dependent: :destroy
+  
 end
---
+++ db/migrate/20130812113023_add_column_to_users.rb
@@ -0,0 +1,7 @@
+class AddColumnToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :username, :string
+    add_column :users, :firstname, :string
+    add_column :users, :lastname, :string
+  end
+end
