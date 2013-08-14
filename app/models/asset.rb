cd47d191933d68d872757ed9df02eb2d2f7f9b6c
tim geaney <timgeaney24@gmail.com>
2013-08-14 20:29:00 +0100

    changed assets download url

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -5,7 +5,10 @@ class Asset < ActiveRecord::Base
 
   has_attached_file :uploaded_file
 
-  validates_attachment_size :uploaded_file, :less_than => 10.megabytes  
+  validates_attachment_size :uploaded_file, 
+                            :less_than => 10.megabytes,
+                            :url => "/assets/get/:id",
+                            :path => ":Rails_root/assets/:id/:basename.:extension"  
   validates_attachment_presence :uploaded_file
 
   def file_name
